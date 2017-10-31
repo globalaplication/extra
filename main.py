e6c2768bd40fc783e41ed0c1c36eac5739d27d84
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-10-31 02:25:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def CutString(line, ref, end='</div>'):
     if (ref == 'a href') : 
@@ -7,11 +7,14 @@ def CutString(line, ref, end='</div>'):
     if (ref == 'img') : 
         ref = '<' + ref
         end = '</img>'
+    if (ref == 'span'):
+        ref = '<' + ref
+        end = '</span>'
     try:
         if line.find(ref) > -1:
             for chr in range(line.find(ref), len(line), +1):
-                if (line[chr:chr+len(ref)] is ref):
-                    line = line.replace(line[chr:line.find(end, chr)+len(end)], '')
+                if (line[chr:chr+len(ref)] == ref):
+                    line = line.replace(line[chr:line.find(end, chr)+len(end)], ' ')
         return line.strip()
     except:
         pass
@@ -21,16 +24,25 @@ def StringCount(line, string):
         if line[chr:chr+len(string)] == string and line[chr:chr+len(string)+1] == string + ' ': 
             count = count + 1
     return count
+def GetCut(line, ref, end='</div>'):
+    for ch in range(len(line), -1, -1):
+        if line[ch:ch+len(ref)] == ref:
+            return line[ch+len(ref):len(line)-len(end)-1]
 def StringReplaceAll(string, old, new, newText=''):
     for chr in string.split(' '):
         if chr == old: newText = newText + new + ' '
         else: newText = newText + chr + ' '
     return newText
-            
 
-line = 'test beta deneme beta'
 
-print(StringCount(line, 'beta'))
-print(StringReplaceAll(line, 'beta', 'replace' ))
+line = 'test beta beta deneme <title> bu bir başlıktır </title> '
+
+print('CutString', CutString(line, '<title>', '</title>'))
+
+print('StringCount', StringCount(line, 'beta'))
+
+print('StringReplaceAll', StringReplaceAll(line, 'beta', 'replace' ))
+
+print('GetCut', GetCut(line, '<title>', '</title>'))
 
 
